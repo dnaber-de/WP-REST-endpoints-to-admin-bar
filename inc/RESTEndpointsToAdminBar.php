71a688b6729d3d33ee28cc63aa652523124f71bc
bueltge <frank@bueltge.de>
2015-10-08 16:35:48 +0200

    Add version switch in dependence from the api version, #7

--- inc/RESTEndpointsToAdminBar.php
+++ inc/RESTEndpointsToAdminBar.php
@@ -14,10 +14,23 @@ class RESTEndpointsToAdminBar {
 	 */
 	public function run() {
 
-		// Set the path base of the Json Api.
-		$this->api_path = '/wp-json/wp/v2/';
+		$this->set_path_to_api_version();
 
 		add_action( 'wp_before_admin_bar_render', [ $this, 'updade_admin_bar' ] );
+	}
+
+	/**
+	 * Set the api path, in dependence from the API version.
+	 */
+	private function set_path_to_api_version() {
+		// Set the path base of the Json Api.
+		// V2 use a new constant for his version number :(.
+		if ( defined( 'REST_API_VERSION' ) ) {
+			$this->api_path = '/wp-json/wp/v2/';
+		} else {
+			// Version 1 as fallback.
+			$this->api_path = '/wp-json/';
+		}
 	}
 
 	/**
